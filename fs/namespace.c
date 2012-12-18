12a6a9b0b896acd9afedc0cc852ed566faed7c35
Dennis Rassmann <showp1984@gmail.com>
2012-12-18 03:52:18 +0100

    fs/namespace: fix: 'warning: 'kernel_type/dir' may be used uninitialized in this function'
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2361,8 +2361,8 @@ SYSCALL_DEFINE5(mount, char __user *, dev_name, char __user *, dir_name,
 		char __user *, type, unsigned long, flags, void __user *, data)
 {
 	int ret;
-	char *kernel_type;
-	char *kernel_dir;
+	char *kernel_type = NULL;
+	char *kernel_dir = NULL;
 	char *kernel_dev = NULL;
 	unsigned long data_page;
 
