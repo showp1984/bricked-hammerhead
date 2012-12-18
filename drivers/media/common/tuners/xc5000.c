f7d6b94ac7cfbe1358ab0d4a2acf92f5a8558032
Dennis Rassmann <showp1984@gmail.com>
2012-12-18 04:14:48 +0100

    drivers/media/common/tuners/xc5000: fix 'warning: 'frame_lines' may be used uninitialized in this function'
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- drivers/media/common/tuners/xc5000.c
+++ drivers/media/common/tuners/xc5000.c
@@ -645,7 +645,7 @@ static void xc_debug_dump(struct xc5000_priv *priv)
 	u32 freq_error_hz = 0;
 	u16 lock_status;
 	u32 hsync_freq_hz = 0;
-	u16 frame_lines;
+	u16 frame_lines = 0;
 	u16 quality = 0;
 	u8 hw_majorversion = 0, hw_minorversion = 0;
 	u8 fw_majorversion = 0, fw_minorversion = 0;
