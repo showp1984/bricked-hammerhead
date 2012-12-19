d8c90d9ee55d2f167fe5d3ef6c4d5a508c0248ff
Dennis Rassmann <showp1984@gmail.com>
2012-12-19 06:45:04 +0100

    msm_mpdecision: fix get_slowest_cpu()
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -87,12 +87,18 @@ static int get_slowest_cpu(void)
         unsigned long rate, slow_rate = 0;
 
         for (i = 0; i < CONFIG_NR_CPUS; i++) {
+                if (!cpu_online(i))
+                        continue;
+
                 rate = get_rate(i);
-                if ((rate < slow_rate) && (slow_rate != 0)) {
-                        cpu = i;
+                if (slow_rate == 0) {
                         slow_rate = rate;
                 }
-                if (slow_rate == 0) {
+
+                if ((rate <= slow_rate) && (slow_rate != 0)) {
+                        if (i == 0)
+                                continue;
+                        cpu = i;
                         slow_rate = rate;
                 }
         }
