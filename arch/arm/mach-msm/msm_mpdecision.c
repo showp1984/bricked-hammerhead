4eaf3e34331cfc36bd0e628b766e7d3f3b7c5257
Dennis Rassmann <showp1984@gmail.com>
2012-12-19 06:46:44 +0100

    msm_mpdecision: decrease startup delay
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -35,7 +35,7 @@
 #define DEBUG 1
 
 #define MPDEC_TAG                       "[MPDEC]: "
-#define MSM_MPDEC_STARTDELAY            70000
+#define MSM_MPDEC_STARTDELAY            20000
 #define MSM_MPDEC_DELAY                 500
 #define MSM_MPDEC_PAUSE                 10000
 #define MSM_MPDEC_IDLE_FREQ             486000
