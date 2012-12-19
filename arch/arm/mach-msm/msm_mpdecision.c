d838b1bbc5d1b063b840e2be41f1e2eb0ab917ae
Dennis Rassmann <showp1984@gmail.com>
2012-12-19 06:46:56 +0100

    msm_mpdecision: decrease decision delay
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -36,7 +36,7 @@
 
 #define MPDEC_TAG                       "[MPDEC]: "
 #define MSM_MPDEC_STARTDELAY            20000
-#define MSM_MPDEC_DELAY                 500
+#define MSM_MPDEC_DELAY                 100
 #define MSM_MPDEC_PAUSE                 10000
 #define MSM_MPDEC_IDLE_FREQ             486000
 
