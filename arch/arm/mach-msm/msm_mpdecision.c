1784c9e4654b9f55aa48cfe1b98ef8007bbee476
Dennis Rassmann <showp1984@gmail.com>
2012-12-21 05:27:34 +0100

    msm_mpdecision: on resume pull up cpu1
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -317,7 +317,20 @@ static void msm_mpdec_resume(void)
 	for_each_possible_cpu(cpu)
 		per_cpu(msm_mpdec_cpudata, cpu).device_suspended = false;
 
+	mutex_lock(&per_cpu(msm_mpdec_cpudata, 1).suspend_mutex);
+	if (!cpu_online(1)) {
+		/* Always enable cpu1 when screen comes online.
+		 * This boosts the wakeup process. */
+		cpu_up(1);
+		per_cpu(msm_mpdec_cpudata, 1).on_time = ktime_to_ms(ktime_get());
+		per_cpu(msm_mpdec_cpudata, 1).online = true;
+		pr_info(MPDEC_TAG"Screen -> on. Hot plugged CPU1 | Mask=[%d%d%d%d]\n",
+                        cpu_online(0), cpu_online(1), cpu_online(2), cpu_online(3));
+	}
+	mutex_unlock(&per_cpu(msm_mpdec_cpudata, 1).suspend_mutex);
+
         /* wake up main work thread */
+        was_paused = true;
         queue_delayed_work(msm_mpdec_workq, &msm_mpdec_work, 0);
 
         pr_info(MPDEC_TAG"Screen -> on. Activated mpdecision. | Mask=[%d%d%d%d]\n",
