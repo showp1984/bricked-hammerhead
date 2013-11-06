e7f0564897ec980ce152177e548f4805dd97ec55
Dennis Rassmann <showp1984@gmail.com>
2013-11-06 02:51:59 +0100

    thermal: raise thermal values back to normal levels
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-msm/lge/board-8974-hammerhead.c
+++ arch/arm/mach-msm/lge/board-8974-hammerhead.c
@@ -92,18 +92,18 @@ static void __init msm8974_early_memory(void)
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 0,
 	.poll_ms = 400,
-	.shutdown_temp = 83,
+	.shutdown_temp = 88,
 
-	.allowed_max_high = 79,
-	.allowed_max_low = 74,
+	.allowed_max_high = 84,
+	.allowed_max_low = 79,
 	.allowed_max_freq = 300000,
 
-	.allowed_mid_high = 76,
-	.allowed_mid_low = 71,
+	.allowed_mid_high = 81,
+	.allowed_mid_low = 76,
 	.allowed_mid_freq = 960000,
 
-	.allowed_low_high = 74,
-	.allowed_low_low = 68,
+	.allowed_low_high = 79,
+	.allowed_low_low = 73,
 	.allowed_low_freq = 1728000,
 };
 #endif
