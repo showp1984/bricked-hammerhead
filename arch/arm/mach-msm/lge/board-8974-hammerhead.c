35ac5c29214dff2d650c3d0c9ff1916f3a2327a4
Dennis Rassmann <showp1984@gmail.com>
2013-11-06 19:00:13 +0100

    update for twrp
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-msm/lge/board-8974-hammerhead.c
+++ arch/arm/mach-msm/lge/board-8974-hammerhead.c
@@ -92,18 +92,18 @@ static void __init msm8974_early_memory(void)
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 0,
 	.poll_ms = 400,
-	.shutdown_temp = 88,
+	.shutdown_temp = 80,
 
-	.allowed_max_high = 84,
-	.allowed_max_low = 79,
+	.allowed_max_high = 74,
+	.allowed_max_low = 69,
 	.allowed_max_freq = 300000,
 
-	.allowed_mid_high = 81,
-	.allowed_mid_low = 76,
+	.allowed_mid_high = 71,
+	.allowed_mid_low = 66,
 	.allowed_mid_freq = 960000,
 
-	.allowed_low_high = 79,
-	.allowed_low_low = 73,
+	.allowed_low_high = 69,
+	.allowed_low_low = 63,
 	.allowed_low_freq = 1728000,
 };
 #endif
