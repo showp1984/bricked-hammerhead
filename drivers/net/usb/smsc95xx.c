f732545132f3cb6b6d6c0e722812884a1f06403d
Dennis Rassmann <showp1984@gmail.com>
2013-11-11 01:18:58 +0100

    drivers/net/usb/smsc95xx.c: fix: warning: 'val' may be used uninitialized in this function
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- drivers/net/usb/smsc95xx.c
+++ drivers/net/usb/smsc95xx.c
@@ -119,7 +119,7 @@ static int smsc95xx_write_reg(struct usbnet *dev, u32 index, u32 data)
 static int smsc95xx_phy_wait_not_busy(struct usbnet *dev)
 {
 	unsigned long start_time = jiffies;
-	u32 val;
+	u32 val = 0;
 
 	do {
 		smsc95xx_read_reg(dev, MII_ADDR, &val);
@@ -196,7 +196,7 @@ static void smsc95xx_mdio_write(struct net_device *netdev, int phy_id, int idx,
 static int smsc95xx_wait_eeprom(struct usbnet *dev)
 {
 	unsigned long start_time = jiffies;
-	u32 val;
+	u32 val = 0;
 
 	do {
 		smsc95xx_read_reg(dev, E2P_CMD, &val);
@@ -216,7 +216,7 @@ static int smsc95xx_wait_eeprom(struct usbnet *dev)
 static int smsc95xx_eeprom_confirm_not_busy(struct usbnet *dev)
 {
 	unsigned long start_time = jiffies;
-	u32 val;
+	u32 val = 0;
 
 	do {
 		smsc95xx_read_reg(dev, E2P_CMD, &val);
@@ -234,7 +234,7 @@ static int smsc95xx_eeprom_confirm_not_busy(struct usbnet *dev)
 static int smsc95xx_read_eeprom(struct usbnet *dev, u32 offset, u32 length,
 				u8 *data)
 {
-	u32 val;
+	u32 val = 0;
 	int i, ret;
 
 	BUG_ON(!dev);
@@ -264,7 +264,7 @@ static int smsc95xx_read_eeprom(struct usbnet *dev, u32 offset, u32 length,
 static int smsc95xx_write_eeprom(struct usbnet *dev, u32 offset, u32 length,
 				 u8 *data)
 {
-	u32 val;
+	u32 val = 0;
 	int i, ret;
 
 	BUG_ON(!dev);
